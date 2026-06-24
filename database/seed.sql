c2a96f01390bd0593bed7b7b8151c1bc4684a4a3
Dee-77 <dinaseid173@gmail.com>
2026-06-24 04:50:28 +0100

    feat(seed): add users and categories seed data

--- database/seed.sql
+++ database/seed.sql
@@ -1,1 +1,33 @@
-﻿
+﻿USE smart_budget;
+
+-- =========================
+-- RESET TABLES (SAFE TESTING)
+-- =========================
+SET FOREIGN_KEY_CHECKS = 0;
+
+TRUNCATE TABLE expenses;
+TRUNCATE TABLE incomes;
+TRUNCATE TABLE budgets;
+TRUNCATE TABLE categories;
+TRUNCATE TABLE users;
+
+SET FOREIGN_KEY_CHECKS = 1;
+
+-- =========================
+-- USERS
+-- =========================
+INSERT INTO users (username, email, password)
+VALUES 
+('admin', 'admin@gmail.com', '1234'),
+('john', 'john@gmail.com', '1234');
+
+-- =========================
+-- CATEGORIES
+-- =========================
+INSERT INTO categories (category_name)
+VALUES 
+('Food'),
+('Transport'),
+('Rent'),
+('Education'),
+('Entertainment');
