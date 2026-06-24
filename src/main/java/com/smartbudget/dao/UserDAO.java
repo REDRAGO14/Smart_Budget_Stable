04daa912852767b3729db57de18d691c604b7762
Dee-77 <dinaseid173@gmail.com>
2026-06-24 05:10:17 +0100

    feat(dao): add database connection setup for UserDAO

--- src/main/java/com/smartbudget/dao/UserDAO.java
+++ src/main/java/com/smartbudget/dao/UserDAO.java
@@ -4,3 +4,8 @@ import com.smartbudget.models.User;
 
 public class UserDAO {
 }
+import com.smartbudget.database.DatabaseConnection;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
